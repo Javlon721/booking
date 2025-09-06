11d140d1d72792e455b22d85d37f40174ff5e51f
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-06 12:28:16 +0500

    add UserUpdateInfo

--- api/src/user/models.py
+++ api/src/user/models.py
@@ -1,19 +1,16 @@
-from datetime import datetime, date
-from decimal import Decimal
-from typing import Annotated
+from datetime import date
 
 from pydantic import BaseModel, Field
 
+from src.utils import SetNonesMixin
+
 
 class UserCreateInfo(BaseModel):
-    first_name: Annotated[str, Field(min_length=2)]
+    first_name: str | None = Field(min_length=2)
     last_name: str | None = None
-    date_of_birth: date
-    phone: Annotated[str, Field(min_length=9, max_length=9)]
+    date_of_birth: date | None
+    phone: str | None = Field(min_length=9, max_length=9)
 
 
-class UserInfo(UserCreateInfo):
-    user_id: int
-    created_at: datetime
-    customer_wallet: Decimal
-    admin_wallet: Decimal
+class UserUpdateInfo(UserCreateInfo, SetNonesMixin):
+    pass
