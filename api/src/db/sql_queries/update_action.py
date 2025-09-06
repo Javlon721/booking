d4fce19abf5778f8f0677e3f9ceac17f25898322
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-06 12:28:41 +0500

    refactor update_row
    add returning parameter to query

--- api/src/db/sql_queries/update_action.py
+++ api/src/db/sql_queries/update_action.py
@@ -1,19 +1,21 @@
 from psycopg.sql import Composed, SQL, Identifier
 
 from src.db.sql_queries.conditions import add_and_conditions
-from src.db.sql_queries.utils import create_column_assignments, is_query_empty
+from src.db.sql_queries.utils import create_column_assignments, is_query_empty, get_returning_values
 
 
 def update_row(columns: list[str], conditions: list[str], table_name: str, *,
-               placeholder_postfix: str = '') -> Composed:
+               placeholder_postfix: str = '', returning: list[str] | None = None) -> Composed:
     and_conditions = add_and_conditions(conditions)
     values = create_column_assignments(columns, SQL(', '), placeholder_postfix=placeholder_postfix)
 
     if is_query_empty(and_conditions) or is_query_empty(values):
         raise ValueError('conditions or values in "update_row" fn must not be empty')
 
-    statement = SQL('update {table_name} set {values} {conditions} returning *')
-    return statement.format(table_name=Identifier(table_name), values=values, conditions=and_conditions)
+    statement = SQL('update {table_name} set {values} {conditions} returning {return_vals}')
+
+    return statement.format(table_name=Identifier(table_name), values=values, conditions=and_conditions,
+                            return_vals=get_returning_values(returning))
 
 
 if __name__ == '__main__':
