dd369c1269bf684c1ea607965ec66a820a12ea15
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-06 12:18:00 +0500

    add SetNonesMixin to utils

--- api/src/utils.py
+++ api/src/utils.py
@@ -1,6 +1,8 @@
 import copy
 from typing import Any
 
+from pydantic import model_validator
+
 
 def list_dict_keys(obj: dict) -> list[Any]:
     return list(obj.keys())
@@ -8,3 +10,13 @@ def list_dict_keys(obj: dict) -> list[Any]:
 
 def deep_copy(obj: Any) -> Any:
     return copy.deepcopy(obj)
+
+
+class SetNonesMixin:
+    @model_validator(mode='before')
+    @classmethod
+    def check_card_number_not_present(cls, data: dict[str, Any]) -> Any:
+        for field in cls.model_fields.keys():
+            if field not in data:
+                data.update({field: None})
+        return data
