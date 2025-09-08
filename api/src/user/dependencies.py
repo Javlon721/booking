a40534a998606d7f5f521e0520e60596fa4cc053
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-08 11:01:43 +0500

    refactor delete endpoint make it dependence & fix get endpoint to return null

---
+++ api/src/user/dependencies.py
@@ -0,0 +1,25 @@
+from typing import Annotated, Callable
+
+from fastapi import HTTPException
+
+from src.auth.dependencies import AuthorizeUserDepends
+from src.auth.models import TokenData
+from src.db.pool_dependency import ConnectionPoolDepends
+from src.db.sql_queries.delete_actions import delete_row
+from src.utils import list_dict_keys
+
+
+def delete_user_data(table: str, returning: str | None = None) -> Callable:
+    def delete_user_dependence(conn_pool: ConnectionPoolDepends,
+                               token_data: Annotated[TokenData, AuthorizeUserDepends]):
+        identify_user = {"user_id": token_data.user_id}
+        query = delete_row(list_dict_keys(identify_user), table, returning=[returning])
+
+        try:
+            with conn_pool.connection() as conn:
+                return conn.execute(query, identify_user).fetchone()
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=500, detail="Something went wrong")
+
+    return delete_user_dependence
--- api/src/user/router.py
+++ api/src/user/router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Form, HTTPException
+from fastapi import APIRouter, Form, HTTPException, Depends
 from psycopg.errors import UniqueViolation, ForeignKeyViolation
 from psycopg.rows import class_row
 
@@ -8,11 +8,11 @@ from src.auth.dependencies import AuthorizeUserDepends
 from src.auth.models import TokenData
 from src.db.pool_dependency import ConnectionPoolDepends
 from src.db.sql_queries.conditions import add_and_conditions
-from src.db.sql_queries.delete_actions import delete_row
 from src.db.sql_queries.insert import insert_into
 from src.db.sql_queries.select_actions import select_from_table
 from src.db.sql_queries.update_action import update_row
 from src.db.sql_queries.utils import concat_sql_queries
+from src.user.dependencies import delete_user_data
 from src.user.models import UserCreateInfo, UserUpdateInfo, UserInfo
 from src.utils import list_dict_keys
 
@@ -67,18 +67,13 @@ def create_new_user_info(
 
 
 @user_router.delete("/delete/info/")
-def delete_user_info(conn_pool: ConnectionPoolDepends, token_data: Annotated[TokenData, AuthorizeUserDepends]):
-    table, returning = 'users_info', "user_id"
+def delete_user_login(data: Annotated[delete_user_data("users_info", "user_id"), Depends()]):
+    return data
 
-    identify_user = {"user_id": token_data.user_id}
-    query = delete_row(list_dict_keys(identify_user), table, returning=[returning])
 
-    try:
-        with conn_pool.connection() as conn:
-            return conn.execute(query, identify_user).fetchone()
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail="Something went wrong")
+@user_router.delete("/delete/login/")
+def delete_user_login(data: Annotated[delete_user_data("users_login", "user_id"), Depends()]):
+    return data
 
 
 @user_router.get("/info/")
@@ -95,6 +90,8 @@ def create_new_user_info(
         with conn_pool.connection() as conn:
             conn.row_factory = class_row(UserInfo)
             result: UserInfo = conn.execute(query, identify_user).fetchone()
+            if not result:
+                return None
             return result.model_dump(exclude_none=True)
     except Exception as e:
         print(e)
