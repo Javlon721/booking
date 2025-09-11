ae5e425ab2cba9f357767fb3f09cee57c56a548f
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-11 11:01:17 +0500

    add put endpoint to property

--- api/src/property/router.py
+++ api/src/property/router.py
@@ -9,8 +9,9 @@ from src.db.pool_dependency import ConnectionPoolDepends
 from src.db.sql_queries.conditions import add_and_conditions
 from src.db.sql_queries.insert import insert_into
 from src.db.sql_queries.select_actions import select_from_table
+from src.db.sql_queries.update_action import update_row
 from src.db.sql_queries.utils import concat_sql_queries
-from src.property.models import PropertyCreateInfo, PropertyInfo
+from src.property.models import PropertyCreateInfo, PropertyInfo, PropertyUpdateInfo
 from src.utils import list_dict_keys
 
 property_router = APIRouter(prefix="/property", tags=["property"])
@@ -36,6 +37,34 @@ def create_property(
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="Something went wrong")
+
+
+@property_router.put("/{property_id}")
+def create_new_user_info(
+        property_id: int,
+        property_info: Annotated[PropertyUpdateInfo, Form()], conn_pool: ConnectionPoolDepends,
+        token_data: Annotated[TokenData, AuthorizeUserDepends]):
+    info = property_info.model_dump(exclude_none=True)
+    if not info:
+        return HTTPException(status_code=400, detail=f"Cannot update property ({property_id}) with empty data")
+
+    identify_property = PropertyInfo.foreign_key(token_data.user_id)
+    identify_property.update({"property_id": property_id})
+
+    query = update_row(
+        list_dict_keys(info),
+        list_dict_keys(identify_property),
+        PROPERTY_TABLE,
+        returning=[RETURNING_VALUE]
+    )
+    info.update(identify_property)
+
+    try:
+        with conn_pool.connection() as conn:
+            return conn.execute(query, info).fetchone()
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Something went wrong")
 
 
 @property_router.get("/")
